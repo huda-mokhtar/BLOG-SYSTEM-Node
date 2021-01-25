32679518d40fdf172853a04fd91ea03668f628e0
huda-mokhtar <hodaheikal6@gmail.com>
2021-01-25 21:41:43 +0200

    last

--- models/User.js
+++ models/User.js
@@ -1,33 +1,27 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const{Schema}=mongoose;
+const { Schema } = mongoose;
 
-const userSchema=new Schema({
-    username: {
-        type: String,
-        unique: true,
-        required:true,
-        maxLength: 140,
-      },
-      password: {
-        type: String,
-        required:true,
-      },
-      firstName: {
-        type: String,
-        maxLength: 140,
-        required:true,
-      },
-      followings:[{
-        type: Schema.Types.ObjectId,
-          ref: 'User'
-      }],
-      followers:[{
-        type: Schema.Types.ObjectId,
-          ref: 'User'
-      }],
-      
-}, {toJSON: {
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    maxLength: 140
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    maxLength: 140,
+    required: true
+  },
+  followers: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+  followings: [{ type: Schema.Types.ObjectId, ref: 'user' }]
+}, {
+  toJSON: {
     transform: (doc, ret, options) => {
       delete ret.password;
       return ret;
@@ -37,21 +31,20 @@ const userSchema=new Schema({
 
 
 userSchema.pre('save', function preSave(next) {
-    this.password = bcrypt.hashSync(this.password, 8);
-    next();
-  });
+  if (this.password)//*** */
+  this.password = bcrypt.hashSync(this.password, 8);
+  next();
+});
 
-  userSchema.pre('findOneAndUpdate', function preSave(next) {
-    if (!this._update.password) {
-      return;
-    }
-    this._update.password = bcrypt.hashSync(this._update.password, 8);
-    next();
-  });
+userSchema.pre('findByIdAndUpdate', function (next) { ///*** */
+  if (this._update.password)
+      this._update.password = bcrypt.hashSync(this._update.password, 8)
+  next()
+})
 
-  userSchema.methods.validatePassword = function validatePassword(password) {
-    return bcrypt.compareSync(password, this.password);
-  };
+userSchema.methods.validatePassword = function validatePassword(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
-const userModel=mongoose.model('User',userSchema);
-module.exports=userModel; 
+const userModel = mongoose.model('User', userSchema);
+module.exports = userModel; 
